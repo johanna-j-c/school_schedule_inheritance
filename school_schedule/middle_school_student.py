b942eb573e3781f9ad55e8f3872dd6f1568f8741
Johanna Cabanban <johanna.cabanban@gmail.com>
2023-04-05 14:34:29 -0400

    Add middle school student child class

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,16 @@
 from .student import Student
 
-# add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+
+    def summary(self):
+        student_summary = super().summary()
+        transportation_message = self.display_transportation_message()
+
+        return "\n".join((student_summary, transportation_message))
+
+    def display_transportation_message(self):
+        has_message = "has" if self.gets_transporation else "doesn't have"
+        return f"{self.name} {has_message} transportation"
